9af95b67031815ab750ec8b749329381d6ba85d0
Renato Cavalcanti <renato@cavalcanti.be>
2023-11-15 13:51:31 +0100

    added status

--- src/main/java/user/registry/api/workflows/UserCreationWorkflow.java
+++ src/main/java/user/registry/api/workflows/UserCreationWorkflow.java
@@ -1,6 +1,5 @@
 package user.registry.api.workflows;
 
-import kalix.javasdk.annotations.GenerateId;
 import kalix.javasdk.annotations.Id;
 import kalix.javasdk.annotations.TypeId;
 import kalix.javasdk.client.ComponentClient;
@@ -29,23 +28,28 @@ public class UserCreationWorkflow extends Workflow<UserCreationWorkflow.State> {
     this.componentClient = componentClient;
   }
 
+  public enum Status {
+    INITIATED,
+    EMAIL_RESERVED,
+    USER_CREATED,
+    EMAIL_CONFIRMED,
+    FAILING,
+    FAILED;
+  }
+
   public record State(String userId,
                       UserEntity.Create createCmd,
-                      boolean userCreated,
-                      boolean emailReserved,
+                      Status status,
                       Optional<String> errorMessage) {
 
-    public State emailIsReserved() {
-      return new State(userId, createCmd, userCreated, true, errorMessage);
-    }
-
-    public State userIsCreated() {
-      return new State(userId, createCmd, true, emailReserved, errorMessage);
+    public State withStatus(Status status) {
+      return new State(userId, createCmd, status, errorMessage);
     }
 
     public State withErrorMessage(String errorMessage) {
-      return new State(userId, createCmd, userCreated, emailReserved, Optional.of(errorMessage));
+      return new State(userId, createCmd, Status.FAILING, Optional.of(errorMessage));
     }
+
   }
 
   @GetMapping("/users/{userId}/creation-workflow")
@@ -57,7 +61,7 @@ public class UserCreationWorkflow extends Workflow<UserCreationWorkflow.State> {
   public Effect<State> start(@PathVariable String userId, @RequestBody UserEntity.Create cmd) {
     logger.info("Starting workflow (id:{})", commandContext().workflowId());
 
-    var state = new State(userId, cmd, false, false, Optional.empty());
+    var state = new State(userId, cmd, Status.INITIATED, Optional.empty());
 
     return effects()
       .updateState(state)
@@ -73,7 +77,8 @@ public class UserCreationWorkflow extends Workflow<UserCreationWorkflow.State> {
     var confirmEmailStepName = "confirm-email";
     var confirmEmail =
       step(confirmEmailStepName)
-        .call(() -> {
+        .call(
+          () -> {
             logger.info("step[{}]: confirming address: '{}'", confirmEmailStepName, currentState().createCmd().email());
             return componentClient
               .forValueEntity(currentState().createCmd.email())
@@ -85,28 +90,27 @@ public class UserCreationWorkflow extends Workflow<UserCreationWorkflow.State> {
           Done.class,
           __ ->
             effects()
-              .updateState(currentState().emailIsReserved())
+              .updateState(currentState().withStatus(Status.EMAIL_CONFIRMED))
               .end());
-
 
     //---------------------------------------------------------------------------------------------
     var createUserStepName = "create-user";
     var createUser =
       step(createUserStepName)
-        .call(
+        .asyncCall(
           UserEntity.Create.class,
           cmd -> {
             logger.info("step[{}]: creating user: {}", createUserStepName, cmd);
             return componentClient
               .forEventSourcedEntity(currentState().userId)
-              .call(UserEntity::createUser).params(cmd);
+              .call(UserEntity::createUser).params(cmd).execute();
           }
         )
         // when done, move to email confirmation
         .andThen(
           Done.class,
           __ -> effects()
-            .updateState(currentState().userIsCreated())
+            .updateState(currentState().withStatus(Status.USER_CREATED))
             .transitionTo(confirmEmail.name()));
 
 
@@ -137,7 +141,7 @@ public class UserCreationWorkflow extends Workflow<UserCreationWorkflow.State> {
             } else {
               logger.info("step[{}]: email '{}' reserved", reserveEmailStepName, currentState().createCmd().email());
               return effects()
-                .updateState(currentState().emailIsReserved())
+                .updateState(currentState().withStatus(Status.EMAIL_RESERVED))
                 .transitionTo(createUser.name(), currentState().createCmd());
             }
           });
@@ -152,11 +156,14 @@ public class UserCreationWorkflow extends Workflow<UserCreationWorkflow.State> {
             logger.info("step[{}]: deleting email reservation: '{}'", deleteEmailReservationStepName, currentState().createCmd().email());
             return componentClient
               .forValueEntity(currentState().createCmd().email())
-              .call(UniqueEmailEntity::delete);
+              .call(UniqueEmailEntity::unReserve);
           }
         )
         // once email reservation is deleted, we can stop the workflow
-        .andThen(Done.class, __ -> effects().end());
+        .andThen(Done.class, __ ->
+          effects()
+            .updateState(currentState().withStatus(Status.FAILED))
+            .end());
 
 
     return workflow()
